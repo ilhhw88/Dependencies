4900cabd00ef4f57150caf906d8d505c6f99d5ab
Hanwo Huang <39773431+ilhhw88@users.noreply.github.com>
2020-03-02 19:10:48 -0500

    Dependencies

---
+++ MovieShop.API/Controllers/GenresController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        public GenresController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetAllGenres (int id)
+        {
+            var genres = new[]
+             {
+                             new {Id = 1, Name = "Action"},
+                             new {Id = 2, Name = "Comedy"},
+                             new {Id = 3, Name = "Thriller"}
+                         };
+            return Ok(genres); 
+        }
+    }
+}
---
+++ MovieShop.API/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        public IActionResult GetMovie()
+        {
+            var genres = new[]
+             {
+                             new {Id = 1, Name = "Action"},
+                             new {Id = 2, Name = "Comedy"},
+                             new {Id = 3, Name = "Thriller"}
+                         };
+            return Ok(genres);
+        }
+    }
+}
